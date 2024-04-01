3b93cf02328d99b6893bc114c6198b973805808f
Jose-Matheusc <josematheus112@gmail.com>
2024-03-31 21:54:08 -0300

    Primeiras configuracoes

---
+++ app/src/main/java/com/example/orgs/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.orgs
+
+import android.app.Activity
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : Activity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+}
